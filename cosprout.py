590c8fce3787313e98293a7f86ebe0d00bd74c43
Kalamojo <kojo22444488888888@gmail.com>
2023-03-24 14:48:18 -0500

    defined routes and pages for about us, team, and impact

--- cosprout.py
+++ cosprout.py
@@ -5,3 +5,15 @@ app = Flask(__name__)
 @app.route("/")
 def home():
     return render_template('index.html')
+
+@app.route("/team", methods=["GET"])
+def team():
+    return render_template('team.html')
+
+@app.route("/about", methods=["GET"])
+def about():
+    return render_template('about.html')
+
+@app.route("/impact", methods=["GET"])
+def impact():
+    return render_template('impact.html')
